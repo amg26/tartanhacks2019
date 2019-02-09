b45079cbc37c380f558f5c439a995d6b8655bcaf
ern23 <43358108+ern23@users.noreply.github.com>
2019-02-08 19:57:12 -0500

    Yeet

--- backend.py
+++ backend.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+'''Create app'''
 app = Flask(__name__)
 
-# New test comment
-# Another comment
+'''Add database'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
