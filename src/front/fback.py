1c061e3eabc7f932b3d3700f007d8bd3848165fd
Andrew Gagen <andrew.m.gagen@gmail.com>
2019-02-08 23:03:22 -0500

    separate database code

--- src/front/fback.py
+++ src/front/fback.py
@@ -1,67 +1,47 @@
 from flask import Flask, request, render_template, session
 from flask.views import MethodView
 from flask_sqlalchemy import SQLAlchemy
+from database import User, Event, db
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'slqclkekql-12cumiojkwfa-fo2i4c2ic4flajkfa'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 '''Add database'''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test5.db'
 
-
-class User(db.Model):
-	username = db.Column(db.String(80), primary_key=True)
-	firstName = db.Column(db.String(80), unique = False, nullable = False)
-	age = db.Column(db.Integer, unique = False, nullable = True)
-	profilePic = db.Column(db.LargeBinary, unique = True, nullable = True)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	passions = db.Column(db.String(80), unique = False, nullable = True)
-	hometown = db.Column(db.String(80), unique = False, nullable = False)
-	bio = db.Column(db.String(300), unique = False, nullable = True)
-	
-	
-
-	def __repr__(self):
-		return '<User %r>' % self.usernamee
-		
-		
-class UserAPI(MethodView):
-
-	def get(self, user_id):
-		if user_id is None:
-			# return a list of users
-			pass
-		else:
-			# expose a single user
-			pass
-
-	def post(self):
-		# create a new user
-		pass
-
-	def delete(self, username):
-		# delete a single user
-		pass
-
-	def put(self, username):
-		# update a single user
-		pass
-		
-		
-		
+with app.app_context():
+	db.init_app(app)
+	db.create_all()
+	db.session.add(Event(name="testevent", joincode="hello"))
+	db.session.commit()
 
 @app.route('/', methods=['GET'])
-def index() :
+def index():
+	usr = User(username="testuser2", firstName="test")
+
+	db.session.add(usr)
+	print(usr.id)
+	db.session.commit()
+	print(usr.id)
+
+	print(User.query.all())
+
 	if session.get('join_code') is not None:
-		return 'you are already a part of: ' + session['join_code']
+		pass
+		#return 'you are already a part of: ' + session['join_code']
 	return render_template('index.html')
 
 @app.route('/join', methods=['POST'])
 def join():
 	# need to check if join code is valid and add user to that event
+	ev = db.session.query(Event).filter(Event.joincode == request.form['join_code']).first()
+
+	if ev is not None:
+		print("that's valid my lad")
+		render_template('testing.html')
+	else:
+		return 'INVALID JOIN CODE'
 
 	session['join_code'] = request.form['join_code']
 	return render_template('testing.html')
