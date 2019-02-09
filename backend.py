695d0e810caa478e5a3b859b05b0a6b3ce7b1a16
ern23 <43358108+ern23@users.noreply.github.com>
2019-02-08 20:17:23 -0500

    Yeet

--- backend.py
+++ backend.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 '''Add database'''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
-
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
