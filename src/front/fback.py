b355d303b1d6fc475f9ce8bcf7dc59b6f9c2975a
Andrew Gagen <andrew.m.gagen@gmail.com>
2019-02-08 21:36:54 -0500

    add join post

--- src/front/fback.py
+++ src/front/fback.py
@@ -4,3 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=['GET'])
 def index() :
 	return render_template('index.html')
+
+@app.route('/join', methods=['POST'])
+def join():
+	return 'nice ' + request.form['join_code']
